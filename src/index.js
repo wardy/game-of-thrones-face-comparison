f89ce283231ae8216176f9c0dcf77d4126b0a909
Chris Ward <christopher.ward2@bskyb.com>
2016-06-17 17:10:47 +0100

    Convert local image upload and send request to face API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import faceApiService from './face-api-service/faceApiService'
+import faceApiService from './face-api-service/faceApiService';
+import convertDataURItoBlob from './utils/convert-data-url-to-blob';
 
 document.addEventListener('DOMContentLoaded', function() {
-  faceApiService();
+  const fileInputElement = document.getElementById('imageInput');
+  fileInputElement.addEventListener('change', (event) => {
+
+    const reader = new FileReader();
+    reader.onerror = (e) => (console.log(e));
+    reader.onabort = function(e) {
+      alert('File read cancelled');
+    };
+    reader.onload = function(e) {
+      faceApiService(convertDataURItoBlob(e.target.result));
+    };
+
+    reader.readAsDataURL(event.target.files[0]);
+  }, false);
+
+
   console.log('document is ready. I can sleep now');
 });
