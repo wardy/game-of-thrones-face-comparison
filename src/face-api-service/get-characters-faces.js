9b215686451bf9b2019d0f6d3a896f329e2cb6bd
wardywork <christopher.ward@sky.uk>
2016-06-25 20:35:33 +0100

    getting face ids of male season one characters

--- src/face-api-service/get-characters-faces.js
+++ src/face-api-service/get-characters-faces.js
@@ -1,5 +1,13 @@
 import gameOfThrones from '../face-lists/game-of-thrones';
+import detectFromURL from './detect-from-url'
 
 export default function getCharactersFaces () {
-  
+  const result = gameOfThrones.seasonOne.male.reduce((acc, cur) => {
+    let newAcc = acc;
+    detectFromURL(cur[Object.keys(cur)]).then((characterFaceId) => {
+      newAcc.push(characterFaceId);
+    });
+    return newAcc;
+  },[]);
+  console.log(result);
 }
