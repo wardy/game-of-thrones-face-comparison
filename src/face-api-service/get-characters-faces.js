15f29664001f3c5b37333401d2d8dc31c02f417d
wardywork <christopher.ward@sky.uk>
2016-06-26 09:31:39 +0100

    restructure json to make dom manipulation easier

--- src/face-api-service/get-characters-faces.js
+++ src/face-api-service/get-characters-faces.js
@@ -3,7 +3,7 @@ import detectFromURL from './detect-from-url'
 
 export default function getCharactersFaces () {
   return Promise.all(gameOfThrones.seasonOne.male.map((currentCharacter) => {
-    return detectFromURL(currentCharacter[Object.keys(currentCharacter)]);
+    return detectFromURL(currentCharacter.imageURL);
   })).then((characters) => {
     return Promise.resolve(characters.map((char, idx) => {
       return Object.assign(char[0], gameOfThrones.seasonOne.male[idx]);
