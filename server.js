b4e318e7ce79ab58b1806a5880b030010ec13a8e
Chris Ward <christopher.ward2@bskyb.com>
2016-07-01 15:58:43 +0100

    Application working with its own server

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
+
+// Server Config
 app.set('views', __dirname);
 app.engine('html', require('ejs').renderFile);
+
+// Middleware
+app.use('/build', express.static('build'));
+app.use('/styles', express.static('src/styles'));
 
 app.get('*', function(req, res) {
   res.render('index.html')
