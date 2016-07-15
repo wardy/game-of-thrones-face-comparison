34c89589748403b75b6aead53d2ed17dfd1ddd9b
wardy <christopher.ward2@bskyb.com>
2016-07-15 17:26:26 +0100

    route configured

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const request = require('request');
+
+const characterJSON = require('./src/face-lists/game-of-thrones.json');
+
+app.set('port', process.env.PORT || 8000);
+
+const rootUrl = `http://localhost:${app.get('port')}`;
 
 // Server Config
 app.set('views', __dirname);
@@ -9,10 +16,24 @@ app.engine('html', require('ejs').renderFile);
 app.use('/build', express.static('build'));
 app.use('/styles', express.static('src/styles'));
 
-app.get('*', function(req, res) {
+app.get('/face-checker', (req, res, next) =>  {
+  request(`${rootUrl}/character-json`, (err, response, body) => {
+    if (err) return next(err);
+    console.log(response, body);
+    res.send('yee haw I\'m a cowboy!');
+  })
+})
+
+app.get('/character-json', (req, res) => {
+  res.send(characterJSON);
+});
+
+app.get('*', (req, res) =>  {
   res.render('index.html')
 });
 
-const server = app.listen(process.env.PORT || 8000, function() {
+const server = app.listen(app.get('port'), () => {
   console.log(`Server is running on ${server.address().port}`)
 });
+
+module.exports = app;
